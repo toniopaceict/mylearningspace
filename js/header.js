c75a710e46392d3639bf5995fdbe392d19e75344
toniopaceict <toniopaceict@gmail.com>
2026-03-27 16:35:26 +0100

    Update header.js

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ function initTonioHeader() {
   const headerPlaceholder = document.getElementById("header-placeholder");
   if (!headerPlaceholder) return;
 
-  fetch("https://toniopaceict.github.io/level4_css/html/header.html?v=1")
+  fetch("https://toniopaceict.github.io/mylearningspace/html/header.html?v=1")
     .then(response => {
       if (!response.ok) {
         throw new Error("Header file could not be loaded.");
