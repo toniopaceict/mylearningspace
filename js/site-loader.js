6cdb3b623d6607b0c609fb03ede667be997b8393
toniopaceict <toniopaceict@gmail.com>
2026-07-22 16:09:10 +0200

    Add files via upload

--- js/site-loader.js
+++ js/site-loader.js
@@ -3,7 +3,7 @@
 
   
   
-  const GLIP_ASSET_VERSION = "325";
+  const GLIP_ASSET_VERSION = "326";
   const GLIP_BASE_URL = "https://toniopaceict.github.io/mylearningspace";
 
   window.GLIP_ASSET_VERSION = GLIP_ASSET_VERSION;
