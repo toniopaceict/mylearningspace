966a8b06534dc70c983a3d1aad5319ba432d0b99
toniopaceict <toniopaceict@gmail.com>
2026-07-28 15:26:49 +0200

    Update site-loader.js

--- js/site-loader.js
+++ js/site-loader.js
@@ -3,7 +3,7 @@
 
   
   
-  const GLIP_ASSET_VERSION = "360";
+  const GLIP_ASSET_VERSION = "361";
   const GLIP_BASE_URL = "https://toniopaceict.github.io/mylearningspace";
 
   window.GLIP_ASSET_VERSION = GLIP_ASSET_VERSION;
