75fcc26cc4eb816247d5c79a106c99c53ca865e1
toniopaceict <toniopaceict@gmail.com>
2026-08-06 17:31:22 +0200

    Update school-config.js

--- js/school-config.js
+++ js/school-config.js
@@ -6,7 +6,7 @@
       label: "St Michael's School",
       webAppUrl: "https://script.google.com/macros/s/AKfycby8gBQU17qcDvWDMLKxcdhzWRcgtscTS7_mr5OYEeQKRoY6l_645znyqaBUhSqIk8Td/exec"
     },
-
+ 
     raphael: {
       label: "St Raphael's School",
       webAppUrl: "https://script.google.com/macros/s/AKfycbyBqXP62z6NBh5QulyefkF-iXXCccBv4w-LvJDWMbIGzAvIBO0cNrobJYtmvmSwQCq5TQ/exec"
